72e27e2dba4273eb80162f7a27f02046b0db7c67
Rodrigo Sebastián Zenteno Martínez <rozenteno@Rodrigos-MacBook-Pro.local>
2019-05-23 01:06:17 -0500

    first commit

---
+++ src/components/RollDice.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Die from "./Die";
+import "../RollDice.css";
+
+export default class RollDice extends Component {
+  static defaultProps = {
+    sides: ["one", "two", "three", "four", "five", "six"]
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      die1: "one",
+      die2: "two",
+      isRolling: false
+    };
+  }
+  roll = () => {
+    const newDie1 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    const newDie2 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    this.setState({ die1: newDie1, die2: newDie2, isRolling: true });
+    setTimeout(() => {
+      this.setState({ isRolling: false });
+    }, 1000);
+  };
+  render() {
+    return (
+      <div className="RollDice">
+        <div className="RollDice-container">
+          <Die face={this.state.die1} rolling={this.state.isRolling} />
+          <Die face={this.state.die2} rolling={this.state.isRolling} />
+        </div>
+        <button onClick={this.roll} disabled={this.state.isRolling}>
+          {this.state.isRolling ? "Rolling..." : "Roll Dice!"}
+        </button>
+      </div>
+    );
+  }
+}
